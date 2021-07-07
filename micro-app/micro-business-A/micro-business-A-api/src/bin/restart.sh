dc429551c945065759bd5e88ca619e30d83f6773
laifeiyang <laify@tydic.com>
2021-07-07 15:10:44 +0800

    架构序列十：如何让一个产品同时适用nacos和zookeeper？

--- micro-app/micro-business-A/micro-business-A-api/src/bin/restart.sh
+++ micro-app/micro-business-A/micro-business-A-api/src/bin/restart.sh
@@ -17,4 +17,14 @@ sh shutdown.sh
 
 # 启动服务
 echo start ${APPLICATION} Application...
-sh startup.sh
+echo 请确认父模块micro-parent的pom中已进行zookeeper和nacos的切换。[Y:已确认,N：未确认]-默认为zookeeper
+read -t 10 -p "请输入是否确认:" confirm
+# 如果未确认，请从新打包
+if [ "$confirm" = "Y" ]; then
+    echo 感谢配合,继续执行中...
+    sh startup.sh
+elif [ "$confirm" = "y" ]; then
+    echo 感谢配合,继续执行中...
+	sh startup.sh
+fi
+echo 请在父模块micro-parent的pom中进行zookeeper和nacos的切换,并从新打包发布
